22e42dc871c43c3bb9b0c5b211429a510c2195a5
poulison <pa46292@gmail.com>
2026-03-10 21:44:59 -0300

    mouse mudou

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,23 +2,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 14f;
-
     public Collider2D leftWall;
     public Collider2D rightWall;
 
     void Update()
     {
-        float move = Input.GetAxis("Horizontal");
-        transform.Translate(Vector2.right * move * speed * Time.deltaTime);
-
         float paddleHalfWidth = GetComponent<BoxCollider2D>().bounds.extents.x;
 
         float minX = leftWall.bounds.max.x + paddleHalfWidth;
         float maxX = rightWall.bounds.min.x - paddleHalfWidth;
 
+        
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10f; 
+
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+
         Vector3 pos = transform.position;
-        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+        pos.x = Mathf.Clamp(worldPos.x, minX, maxX);
+
         transform.position = pos;
     }
 }
